8ace2637aa0235773672c1e08fd489382566f632
Robert Savage <robert@savage7.com>
2017-08-06 11:59:53 -0400

    updated SPI API & impl to match current WiringPi version

--- wiringPi/wiringPiSPI.h
+++ wiringPi/wiringPiSPI.h
@@ -28,6 +28,7 @@ extern "C" {
 
 int wiringPiSPIGetFd  (int channel) ;
 int wiringPiSPIDataRW (int channel, unsigned char *data, int len) ;
+int wiringPiSPISetupMode (int channel, int speed, int mode) ;
 int wiringPiSPISetup  (int channel, int speed) ;
 
 #ifdef __cplusplus
